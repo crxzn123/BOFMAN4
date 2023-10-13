fab0e75479faed42675411f80156e77e6257cff0
Daoa <daffasyafitra12@gmail.com>
2023-10-13 20:18:02 +0700

    nambahin whitenoise

--- BOFMAN4/urls.py
+++ BOFMAN4/urls.py
@@ -2,8 +2,15 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 
+from django.conf.urls.static import static
+from django.conf import settings
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.landing_view, name='landing'),
     path('about', views.about_view, name='about'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
