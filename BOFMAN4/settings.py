4abec2c34fa7f64ee3af9fbb5b60ecf2d7653aca
Daoa <daffasyafitra12@gmail.com>
2023-10-13 20:38:39 +0700

    tambah allowed host sama middleware

--- BOFMAN4/settings.py
+++ BOFMAN4/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-oxp-@g49sxr91z$+sv9qp3nuks8pv(2rp2cpsezu1^vwse*f6_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
