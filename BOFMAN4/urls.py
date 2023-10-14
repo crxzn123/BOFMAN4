af5c897bdfd381fb4a4599d7d0076069bacb6805
Yafi Alifuddin <yalif1710@gmail.com>
2023-10-14 07:07:48 +0700

    update about,galeri,merch,performance,index,urls,views,navbar

--- BOFMAN4/urls.py
+++ BOFMAN4/urls.py
@@ -9,6 +9,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.landing_view, name='landing'),
     path('about', views.about_view, name='about'),
+    path('gallery', views.gallery_view, name='gallery'),
+    path('merch', views.merch_view, name='merch'),
+    path('perform', views.perform_view, name='perform'),
 ]
 
 if settings.DEBUG:
--- BOFMAN4/views.py
+++ BOFMAN4/views.py
@@ -9,3 +9,18 @@ def about_view(request):
     template = "about.html"
     contexts = {}
     return render(request, template, contexts)
+
+def gallery_view(request):
+    template = "gallery.html"
+    contexts = {}
+    return render(request, template, contexts)
+
+def merch_view(request):
+    template = "merch.html"
+    contexts = {}
+    return render(request, template, contexts)
+
+def perform_view(request):
+    template = "perform.html"
+    contexts = {}
+    return render(request, template, contexts)
